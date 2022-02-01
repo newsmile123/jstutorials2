563873a5186554d8d58400db522a71818474866a
Vasily Agafonov <vasilyagafonov@MacBook-Air-Vasily.local>
2022-02-02 02:44:42 +0300

    first tutorial

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+
+const numberOfFilms = prompt('сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//           personalMovieDB.movies[a] = b;
+// }
+
+
+// console.log(personalMovieDB);
+
+
+
